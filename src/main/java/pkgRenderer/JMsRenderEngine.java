894444b67c24f485b92f0048fc298d1c85d78272
JMSagum <112904712+JMSGM@users.noreply.github.com>
2024-10-24 21:43:30 -0700

    add setter methods in RenderEngine

--- src/main/java/pkgRenderer/JMsRenderEngine.java
+++ src/main/java/pkgRenderer/JMsRenderEngine.java
@@ -9,30 +9,62 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11C.glClear;
 
 public abstract class JMsRenderEngine {
-    private final int DEFAULT_DELAY = 200;
-    private final int DEFAULT_ROWS = 30;
-    private final int DEFAULT_COLS = 30;
-    private final int DEFAULT_SIDES = 20;
+    //Fields
+    private int DEFAULT_DELAY = 500;
+    private int DEFAULT_ROWS = 30;
+    private int DEFAULT_COLS = 30;
+    private int DEFAULT_SIDES = 30;
+    private int DEFAULT_POLYGON_AMOUNT = 20;
     JMsWindowManager my_wm;
     private final Random myRandom = new Random();
+    //Methods
+    abstract void renderPolygons(float cx, float cy, int sides, float radius);
+    abstract void renderRandomPolygons(int polyAmount);
+    protected abstract void generatePolygonArray(int row, int cols, int sides);
+    protected abstract void generatePolygonArray(float radius, int sides);
+    protected abstract void generatePolygonArray(int row, int col);
+    protected  abstract void renderRandomObject(JMsPolygon polygonObj);
+    protected abstract JMsPolygon createRandomObject();
     public void initOpenGL(JMsWindowManager my_wm) {
         this.my_wm = my_wm;
         my_wm.updateContextToThis();
     }
+    //Render Methods
     public void render() {
-        // Generate the random polygon once, before entering the rendering loop
-        JMsPolygon randomPolygon = createRandomObject();
 
         while (!my_wm.isGlfwWindowClosed()) {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // Render the previously created random polygon
-            drawRandomObject(randomPolygon);
+            for (int sides = 3; sides <= DEFAULT_SIDES; sides++) {
+                // Check if the window should close during each frame
+                if (my_wm.isGlfwWindowClosed()) {
+                    break;
+                }
 
-            my_wm.swapBuffers();
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                float R = myRandom.nextFloat();
+                float G = myRandom.nextFloat();
+                float B = myRandom.nextFloat();
+                float OPAC = myRandom.nextFloat();
+                glColor4f(R, G, B, OPAC);
+
+                generatePolygonArray(DEFAULT_ROWS, DEFAULT_COLS, sides);
+
+                my_wm.swapBuffers();
+
+                glfwPollEvents();
+
+                if (DEFAULT_DELAY > 0) {
+                    try {
+                        Thread.sleep(DEFAULT_DELAY);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
         }
-
         my_wm.destroyGLFWWindow();
     }
     public void render(float radius) {
@@ -110,15 +142,19 @@ public abstract class JMsRenderEngine {
         }
         my_wm.destroyGLFWWindow();
     }
-    abstract void drawPolygons(float cx, float cy, int sides, float radius);
-    abstract void renderRandomPolygons(int polyAmount);
-    protected abstract void generatePolygonArray(int row, int cols, int sides);
-    protected abstract void generatePolygonArray(float radius, int sides);
-    protected abstract void generatePolygonArray(int row, int col);
-    protected  abstract void drawRandomObject(JMsPolygon polygonObj);
-    protected abstract JMsPolygon createRandomObject();
-
-
+    //setter and getters for default values
+    protected void setDEFAULT_ROWS(int rows){
+        this.DEFAULT_ROWS = rows;
+    }
+    protected void setDEFAULT_COLS(int cols){
+        this.DEFAULT_COLS = cols;
+    }
+    protected void setDEFAULT_DELAY(int delay){
+        this.DEFAULT_DELAY = delay;
+    }
+    protected void setDEFAULT_POLYGON_AMOUNT(int polyAmount){
+        this.DEFAULT_POLYGON_AMOUNT = polyAmount;
+    }
 
 
 
