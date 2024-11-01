8b230d8de06eca5ceacc4c1aec1db260554d7639
JMSagum <112904712+JMSGM@users.noreply.github.com>
2024-10-31 19:54:21 -0700

    New random object, Implement constructor that bound value in each array cell

--- src/main/java/pkgUtils/JMsPingPong.java
+++ src/main/java/pkgUtils/JMsPingPong.java
@@ -1,6 +1,9 @@
 package pkgUtils;
 
+import java.util.Random;
+
 public class JMsPingPong {
+    private final Random myRandom = new Random();
     private int[][] LIVE_ARR;
     private int[][] NEXT_ARR;
     JMsPingPong(int rows, int cols){
@@ -17,7 +20,13 @@ public class JMsPingPong {
             }
     }
     JMsPingPong(int rows, int cols, int lower, int upper){
-
+        LIVE_ARR = new int[rows][cols];
+        NEXT_ARR = new int[rows][cols];
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                LIVE_ARR[row][col] = myRandom.nextInt(upper-lower) + lower;
+            }
+        }
     }
 
     void setArray(int count){
