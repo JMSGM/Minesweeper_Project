f627daea9ddd7ee1a92047adfec8d70800eefc4e
JMSagum <112904712+JMSGM@users.noreply.github.com>
2024-10-31 21:39:28 -0700

    Implement countNearestNeighbor()

--- src/main/java/pkgUtils/JMsPingPong.java
+++ src/main/java/pkgUtils/JMsPingPong.java
@@ -5,15 +5,21 @@ import java.util.Random;
 public class JMsPingPong {
     //Field
     private final Random myRandom = new Random();
+    private int ROWS;
+    private int COLS;
     private int[][] LIVE_ARR;
     private int[][] NEXT_ARR;
 
     //Constructors
     JMsPingPong(int rows, int cols){
+        ROWS = rows;
+        COLS = cols;
         LIVE_ARR = new int[rows][cols];
         NEXT_ARR = new int[rows][cols];
     }
     JMsPingPong(int rows, int cols, int value){
+        ROWS = rows;
+        COLS = cols;
         LIVE_ARR = new int[rows][cols];
         NEXT_ARR = new int[rows][cols];
         for(int row = 0; row < rows; row++){
@@ -23,6 +29,8 @@ public class JMsPingPong {
             }
     }
     JMsPingPong(int rows, int cols, int lower, int upper){
+        ROWS = rows;
+        COLS = cols;
         LIVE_ARR = new int[rows][cols];
         NEXT_ARR = new int[rows][cols];
         for(int row = 0; row < rows; row++){
@@ -43,8 +51,29 @@ public class JMsPingPong {
     LIVE_ARR = NEXT_ARR;
     NEXT_ARR = tmp;
     }
-    int countNearestNeighbor(){
-       return 0;
+    //count nearest neighbor of specified cell [row][col]
+    int countNearestNeighbor(int row, int col){
+        int count = 0;
+
+        int prevRow = (row - 1 + ROWS) % ROWS;
+        int nextRow = (row + 1) % ROWS;
+        int prevCol = (col - 1 + COLS) % COLS;
+        int nextCol = (col + 1) % COLS;
+
+        if (LIVE_ARR[prevRow][col] == 1){
+            count += 1;
+        }
+        if (LIVE_ARR[nextCol][col] == 1){
+            count += 1;
+        }
+        if (LIVE_ARR[row][prevCol] == 1){
+            count += 1;
+        }
+        if (LIVE_ARR[row][nextCol] == 1){
+            count += 1;
+        }
+
+       return count;
     }
     int countNextNearestNeighbor(){
         return 0;
